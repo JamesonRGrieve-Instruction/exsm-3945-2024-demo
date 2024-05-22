1bccbb233c3611a89014a991196aebb4ffefc589
James Grieve <jamesonrgrieve@gmail.com>
2024-05-21 19:35:23 -0600

    Move forecast into controller;

---
+++ src/DotNetAPIDemo/Controllers/WeatherController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/weather")]
+[ApiController]
+public class WeatherController : ControllerBase
+{
+    string[] summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+
+    [HttpGet()]
+    public ActionResult<WeatherForecast[]> GetWeather()
+    {
+        WeatherForecast[] forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+        return forecast;
+    }
+}
