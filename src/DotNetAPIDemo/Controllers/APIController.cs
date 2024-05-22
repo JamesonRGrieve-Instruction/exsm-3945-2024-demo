4f386d2d8f20a3af00abe12eb1021538455da093
James Grieve <jamesonrgrieve@gmail.com>
2024-05-21 19:38:19 -0600

    Endpoint methods;

--- src/DotNetAPIDemo/Controllers/APIController.cs
+++ src/DotNetAPIDemo/Controllers/APIController.cs
@@ -7,6 +7,30 @@ public class APIController : ControllerBase
     [HttpGet("sample")]
     public ActionResult<string> GetSample()
     {
-        return "Hello, World!";
+        return "Hello, World! - GET";
+    }
+
+    [HttpPost("sample")]
+    public ActionResult<string> PostSample()
+    {
+        return "Hello, World! - POST";
+    }
+
+    [HttpPut("sample")]
+    public ActionResult<string> PutSample()
+    {
+        return "Hello, World! - PUT";
+    }
+
+    [HttpPatch("sample")]
+    public ActionResult<string> PatchSample()
+    {
+        return "Hello, World! - PATCH";
+    }
+
+    [HttpDelete("sample")]
+    public ActionResult<string> DeleteSample()
+    {
+        return "Hello, World! - DELETE";
     }
 }
