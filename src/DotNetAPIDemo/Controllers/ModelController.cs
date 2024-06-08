b4abdca993d9bfe6e1526af20833d2537a577787
James Grieve <jamesonrgrieve@gmail.com>
2024-06-07 20:16:20 -0600

    Add controllers;

---
+++ src/DotNetAPIDemo/Controllers/ModelController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DotNetAPIDemo.Data;
+using DotNetAPIDemo.Models;
+
+namespace DotNetAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ModelController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Model
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Model>>> GetModels()
+        {
+            return await _context.Models.ToListAsync();
+        }
+
+        // GET: api/Model/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Model>> GetModel(int id)
+        {
+            var model = await _context.Models.FindAsync(id);
+
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            return model;
+        }
+
+        // PUT: api/Model/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutModel(int id, Model model)
+        {
+            if (id != model.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(model).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Model
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Model>> PostModel(Model model)
+        {
+            _context.Models.Add(model);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetModel", new { id = model.ID }, model);
+        }
+
+        // DELETE: api/Model/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteModel(int id)
+        {
+            var model = await _context.Models.FindAsync(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            _context.Models.Remove(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ModelExists(int id)
+        {
+            return _context.Models.Any(e => e.ID == id);
+        }
+    }
+}
